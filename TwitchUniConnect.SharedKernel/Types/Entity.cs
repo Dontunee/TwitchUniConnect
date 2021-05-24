22b4c1698f45e5e70ffea6c43891a29b51b5ae49
Babatunde Afolabi <tundeafolabi@users.noreply.github.com>
2021-05-24 22:58:45 +0100

    Just implemented entity baseclass and Value object base class

---
+++ TwitchUniConnect.SharedKernel/Types/Entity.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace TwitchUniConnect.SharedKernel.Types
+{
+    public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    {
+        #region Constructors and properties 
+
+        protected Entity(TId id)
+        {
+            if (!IsIdValid(id)) throw new ArgumentException();
+            Id = id;
+        }
+
+        public TId Id { get; private set; }
+
+
+        #endregion
+
+
+        #region IEquatable implementation
+
+        public bool Equals(Entity<TId> other)
+        {
+            return Id.GetHashCode() == other.Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Entity<TId>);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(Entity<TId> lhs, Entity<TId> rhs)
+        {
+
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(Entity<TId> lhs, Entity<TId> rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+
+        #endregion
+
+        #region Private methods 
+
+        private bool IsIdValid(TId id)
+        {
+            return id is int || id is Guid;
+        }
+
+        #endregion
+
+
+
+
+
+
+    }
+}
---
+++ TwitchUniConnect.SharedKernel/Types/ValueObject.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace TwitchUniConnect.SharedKernel.Types
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        protected abstract IEnumerable<object> GetAtomicValues();
+
+        #region IEquatable implementations 
+        public bool Equals(ValueObject other)
+        {
+            if (other == null || other.GetType() != GetType())
+                return false;
+
+            //getValues in Enumerator of this value object 
+            var thisValues = GetAtomicValues().GetEnumerator();
+
+            //get values in Enumerator of input value object
+            var otherValues = other.GetAtomicValues().GetEnumerator();
+
+            while(thisValues.MoveNext() && otherValues.MoveNext())
+            {
+                //null check for reference values
+                if (ReferenceEquals(thisValues.Current, null)
+                        ^ ReferenceEquals(otherValues.Current, null))
+                    return false;
+
+                //check if current object value isnt empty and other value doesnt equal it
+                if (thisValues.Current != null
+                        && !thisValues.Current.Equals(otherValues.Current))
+                    return false;
+
+            }
+
+            return !thisValues.MoveNext() && otherValues.MoveNext();
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            return Equals(obj as ValueObject);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetAtomicValues()
+                        .Select(x => x != null ? x.GetHashCode() : 0)
+                            .Aggregate((x, y) => x ^ y);
+        }
+
+        public static bool operator ==(ValueObject leftHand, ValueObject rightHand)
+        {
+            //returns true only if one of them is null and the other isnt 
+            //wont return true if both are null
+            if (ReferenceEquals(leftHand, null) ^
+                    ReferenceEquals(rightHand, null))
+                return false;
+
+
+            //if left isnt null,return comparison of left and right
+            return ReferenceEquals(leftHand, null) || leftHand.Equals(rightHand);
+        }
+
+        public static bool operator !=(ValueObject leftHand, ValueObject rightHand)
+        {
+            return !(leftHand == rightHand);
+        }
+
+
+        #endregion
+
+
+    }
+}
